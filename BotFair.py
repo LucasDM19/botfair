fc0311760579de5251d8a09ec5c312f0bf6e0a87
LucasDM19 <lucasmuzel@gmail.com>
2020-08-26 11:08:54 -0300

    ops
    vacilei

--- BotFair.py
+++ BotFair.py
@@ -396,7 +396,7 @@ class BotFair():
                      # if( retorno_aposta["status"] != "SUCCESS" ): #'result' not in retorno_aposta or 
                         # print("Erro:", retorno_aposta)
                      # elif( retorno_aposta['instructionReports'][0]['sizeMatched'] == 0.0 ) :
-                        print("Aposta não correspondida") #Exemplo: {'status': 'SUCCESS', 'marketId': '1.172153457', 'instructionReports': [{'status': 'SUCCESS', 'instruction': {'selectionId': 47972, 'handicap': 0.0, 'limitOrder': {'size': 5.0, 'price': 4.3, 'persistenceType': 'LAPSE'}, 'orderType': 'LIMIT', 'side': 'BACK'}, 'betId': '208573543824', 'placedDate': '2020-08-16T17:19:13.000Z', 'averagePriceMatched': 0.0, 'sizeMatched': 0.0, 'orderStatus': 'EXECUTABLE'}]}
+                        # print("Aposta não correspondida") #Exemplo: {'status': 'SUCCESS', 'marketId': '1.172153457', 'instructionReports': [{'status': 'SUCCESS', 'instruction': {'selectionId': 47972, 'handicap': 0.0, 'limitOrder': {'size': 5.0, 'price': 4.3, 'persistenceType': 'LAPSE'}, 'orderType': 'LIMIT', 'side': 'BACK'}, 'betId': '208573543824', 'placedDate': '2020-08-16T17:19:13.000Z', 'averagePriceMatched': 0.0, 'sizeMatched': 0.0, 'orderStatus': 'EXECUTABLE'}]}
                         # bet_id = retorno_aposta['instructionReports'][0]['betId'] # Salva o Id da aposta
                         # filtro='{"betId" : '+str(bet_id)+' }'
                         # retorno_cancelamento = self.api.cancelaAposta(json_req=filtro) # Exempo: {'status': 'SUCCESS', 'instructionReports': []}
